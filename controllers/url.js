da3c17f9547f60b941b01d087623cee2e5a8c788
yojina <yojinaaryal2064@gmail.com>
2025-11-05 21:46:02 +0545

    controller and models

---
+++ controllers/url.js
@@ -0,0 +1,16 @@
+const {nanoid}=require("nanoid");
+const URL=require("../models/url")
+async function generateNewShortURL(req,res){
+    const body=req.body;
+    if(!body.url) return res.status(400).json({error: "url is required"});
+    const shortID=nanoid(8);
+    await URL.create({
+        shortId: shortID,
+        redirectURl: body.url,
+        visitHistory: [],
+});
+return res.json({id: shortID})
+}
+module.exports={
+    generateNewShortURL,
+}
---
+++ routes/url.js
@@ -0,0 +1,6 @@
+const express=require("express");
+const {generateNewShortURL}=require("../controllers/url");
+const router=express.Router();
+
+router.post("/",generateNewShortURL);
+module.exports=router;
