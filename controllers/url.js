ae00ad6a9af0b4b90170264b35303e7aa8a04b13
yojina <yojinaaryal2064@gmail.com>
2025-11-07 22:27:06 +0545

    url analytics

--- controllers/url.js
+++ controllers/url.js
@@ -11,6 +11,13 @@ async function generateNewShortURL(req,res){
 });
 return res.json({id: shortID})
 }
+async function getAnalytics(req,res){
+    const shortId=req.params.shortId;
+    const result = await URL.findOne({shortId});
+    return res.json({totalClicks:result.visitHistory.length,
+        analytics: result.visitHistory})
+}
 module.exports={
     generateNewShortURL,
+    getAnalytics, 
 }
--- routes/url.js
+++ routes/url.js
@@ -1,17 +1,8 @@
 const express=require("express");
-const {generateNewShortURL}=require("../controllers/url");
+const {generateNewShortURL,getAnalytics}=require("../controllers/url");
 const router=express.Router();
-router.delete("/:shortId", async (req, res) => {
-  const { shortId } = req.params;
-  const result = await URL.deleteOne({ shortId });
-
-  if (result.deletedCount === 0) {
-    return res.status(404).json({ error: "Short URL not found" });
-  }
-
-  return res.json({ message: "Short URL deleted successfully" });
-});
 
 
 router.post("/",generateNewShortURL);
+router.get("/analytics/:shortId",getAnalytics);
 module.exports=router;
