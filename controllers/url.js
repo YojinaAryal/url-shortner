d01dea9318071cd97355bcb05fc5fe426ab3f54b
yojina <yojinaaryal2064@gmail.com>
2025-11-08 20:56:13 +0545

    url shortner with ui

--- controllers/url.js
+++ controllers/url.js
@@ -4,12 +4,16 @@ async function generateNewShortURL(req,res){
     const body=req.body;
     if(!body.url) return res.status(400).json({error: "url is required"});
     const shortID=shortid();
+
     await URL.create({
         shortId: shortID,
         redirectURL: body.url,
         visitHistory: [],
 });
-return res.json({id: shortID})
+return res.render("home",{
+    id: shortID,
+});
+
 }
 async function getAnalytics(req,res){
     const shortId=req.params.shortId;
--- index.js
+++ index.js
@@ -1,19 +1,35 @@
-const express = require("express");
+ const express = require("express");
+const path=require("path")
 const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/url");
+const staticRoute=require("./routes/staticrouter")
 const URL = require("./models/url");
 const app = express(); 
 const port = 8000;
 
-app.use(express.json()); 
+
 
 connectToMongoDB("mongodb://localhost:27017/short-url")
   .then(() => console.log("MongoDB connected!"));
 
+app.set("view engine","ejs");
+app.set("views",path.resolve("./views"));
 
+app.use(express.json()); 
+app.use(express.urlencoded({extended: false}))
+
+app.get("/test",async(req,res)=>{
+  const allUrls=await URL.find({});
+  return res.render("home",{
+    urls:allUrls,
+    
+  });
+})
 app.use("/url", urlRoute);
+app.use("/",staticRoute);
 app.get("/:shortId",async (req,res)=>{
  const shortId=req.params.shortId;
+
  const entry= await URL.findOneAndUpdate({
   shortId
  },{$push:{
